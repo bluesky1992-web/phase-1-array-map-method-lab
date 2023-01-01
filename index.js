7b56b5840ef0eea2b421610dc838c67681213c67
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-01-01 16:43:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
-}
+  const titleCase = [];
+  for (let i = 0; i < tutorials.length; i++) {
+      const words = tutorials[i].split(" ");
+    const convert = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+      titleCase.push(convert);
+  }
+  console.log(titleCase);
+  return titleCase;
+};
+
+titleCased();
+
+
+
+
+
